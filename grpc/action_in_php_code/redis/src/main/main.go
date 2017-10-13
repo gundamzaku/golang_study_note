e72ffdd722d4732b141b3a85b1767bc40eb8357c
gundamzaku <gundamzaku@163.com>
2017-10-13 22:45:53 +0800

    add demo

---
+++ grpc/action_in_php_code/redis/src/main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	pb "proto"
+	"net"
+	"log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) Command(ctx context.Context, in *pb.RedisRequest) (*pb.RedisReply, error) {
+	return &pb.RedisReply{Result: "Action is "+in.Action+"  Param is "+in.Param }, nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterRedisServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
