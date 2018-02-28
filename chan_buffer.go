22ee2dd0c2821de1cb46fe648e2f197caa555e49
中原一点点 <4845327@qq.com>
2018-02-28 10:55:54 +0800

    Update chan_buffer.go

--- chan_buffer.go
+++ chan_buffer.go
@@ -1,1 +1,31 @@
-123
+如何理解有缓存的通道和无缓存的通道  
+
+```go
+func main() {
+
+	//创建一个通道
+	c := make(chan int)
+	c<-100
+	c<-99
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
+```
+`
+fatal error: all goroutines are asleep - deadlock!
+
+goroutine 1 [chan send]:
+`
+
+```go
+func main() {
+
+	//创建一个通道
+	c := make(chan int,2)
+	c<-100
+	c<-99
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
+```
+正常
